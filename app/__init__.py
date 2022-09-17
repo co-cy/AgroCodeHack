92626d7e7e5555b6a396fe6034d09fcfe6551cdb
_co_cy_ <stf2003.kem@gmail.com>
2022-09-17 18:14:46 +0300

    [develop] -> Fix cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import UvicornConfig
 from app import database, routes
 from fastapi import FastAPI
 from asyncio import run
 import uvicorn
 
+origins = [
+    "*"
+]
 
 run(database.init_database())
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 routes.init_routes(app)
 uvicorn.run(app, **UvicornConfig())
