795d31b50f792f92bfd61d14a5a546919faae245
_co_cy_ <stf2003.kem@gmail.com>
2022-09-17 19:44:39 +0300

    [develop] -> The get_map functionality is exposed

--- app/routes/get_map.py
+++ app/routes/get_map.py
@@ -36,23 +36,33 @@ def get_map(req: MapRequest):
         top_left_x = top_left_chunk_top_left_x
         top_left_y = top_left_chunk_top_left_y
         if top_left_chunk_top_left_x == bottom_right_chunk_top_left_x and top_left_chunk_top_left_y == bottom_right_chunk_top_left_y:
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 1023}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 1023}_{req.scale}',
+                    'rb') as f:
                 data = pickle.load(f)
         else:
             size = 4
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 1023}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 1023}_{req.scale}',
+                    'rb') as f:
                 data = pickle.load(f)
             top_right_chunk = None
-            with open(f'./chunks/{top_left_chunk_top_left_x+1024}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 2047}_{top_left_chunk_top_left_y + 1023}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x + 1024}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 2047}_{top_left_chunk_top_left_y + 1023}_{req.scale}',
+                    'rb') as f:
                 top_right_chunk = pickle.load(f)
             bottom_left_chunk = None
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y+1024}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 2047}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y + 1024}__{top_left_chunk_top_left_x + 1023}_{top_left_chunk_top_left_y + 2047}_{req.scale}',
+                    'rb') as f:
                 bottom_left_chunk = pickle.load(f)
             bottom_right_chunk = None
-            with open(f'./chunks/{bottom_right_chunk_top_left_x}_{bottom_right_chunk_top_left_y}__{bottom_right_chunk_top_left_x + 1023}_{bottom_right_chunk_top_left_y + 1023}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{bottom_right_chunk_top_left_x}_{bottom_right_chunk_top_left_y}__{bottom_right_chunk_top_left_x + 1023}_{bottom_right_chunk_top_left_y + 1023}_{req.scale}',
+                    'rb') as f:
                 bottom_right_chunk = pickle.load(f)
             data = np.concatenate((data, top_right_chunk), axis=1)
-            bottom_left_chunk = np.concatenate((bottom_left_chunk,bottom_right_chunk), axis=1)
+            bottom_left_chunk = np.concatenate((bottom_left_chunk, bottom_right_chunk), axis=1)
             data = np.concatenate((data, bottom_left_chunk), axis=0)
     elif req.scale == 1:
         top_left_chunk_top_left_x = req.corners[0].x // 128 * 128
@@ -63,23 +73,33 @@ def get_map(req: MapRequest):
         top_left_x = top_left_chunk_top_left_x
         top_left_y = top_left_chunk_top_left_y
         if top_left_chunk_top_left_x == bottom_right_chunk_top_left_x and top_left_chunk_top_left_y == bottom_right_chunk_top_left_y:
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}',
+                    'rb') as f:
                 data = pickle.load(f)
         else:
             size = 4
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}',
+                    'rb') as f:
                 data = pickle.load(f)
             top_right_chunk = None
-            with open(f'./chunks/{top_left_chunk_top_left_x+128}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 255}_{top_left_chunk_top_left_y + 127}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x + 128}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 255}_{top_left_chunk_top_left_y + 127}_{req.scale}',
+                    'rb') as f:
                 top_right_chunk = pickle.load(f)
             bottom_left_chunk = None
-            with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y+128}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 255}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y + 128}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 255}_{req.scale}',
+                    'rb') as f:
                 bottom_left_chunk = pickle.load(f)
             bottom_right_chunk = None
-            with open(f'./chunks/{bottom_right_chunk_top_left_x}_{bottom_right_chunk_top_left_y}__{bottom_right_chunk_top_left_x + 127}_{bottom_right_chunk_top_left_y + 127}_{req.scale}', 'rb') as f:
+            with open(
+                    f'./chunks/{bottom_right_chunk_top_left_x}_{bottom_right_chunk_top_left_y}__{bottom_right_chunk_top_left_x + 127}_{bottom_right_chunk_top_left_y + 127}_{req.scale}',
+                    'rb') as f:
                 bottom_right_chunk = pickle.load(f)
             data = np.concatenate((data, top_right_chunk), axis=1)
-            bottom_left_chunk = np.concatenate((bottom_left_chunk,bottom_right_chunk), axis=1)
+            bottom_left_chunk = np.concatenate((bottom_left_chunk, bottom_right_chunk), axis=1)
             data = np.concatenate((data, bottom_left_chunk), axis=0)
         pass
     else:
@@ -89,21 +109,31 @@ def get_map(req: MapRequest):
     for pixel in req.pixels:
         top_left_chunk_top_left_x = pixel.x // 128 * 128
         top_left_chunk_top_left_y = pixel.y // 128 * 128
-        with open(f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}', 'rb') as f:
-            pixels_data.append(pickle.load(f)[pixel.x%128][pixel.y%128])
+        with open(
+                f'./chunks/{top_left_chunk_top_left_x}_{top_left_chunk_top_left_y}__{top_left_chunk_top_left_x + 127}_{top_left_chunk_top_left_y + 127}_{req.scale}',
+                'rb') as f:
+            pixels_data.append(pickle.load(f)[pixel.x % 128][pixel.y % 128])
 
     sim = cosine_similarity(pixels_data, data.reshape(-1, data.shape[-1]))
     sim[sim < 0] = 0
     sim = sim.mean(axis=0)
     sim = sim.reshape(data.shape[0], data.shape[1])
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            if data[x][y][0] == 0:
+                sim[x][y] = -1
+            elif data[x][y][7] == 1:
+                sim[x][y] = 2
+            elif data[x][y][1] == 7:
+                sim[x][y] = -2
 
     return {
-            "data": sim.tolist(),
-            "params": {
-                "top_left_coords": {
-                    "x": top_left_x,
-                    "y": top_left_y
-                },
-                "size": size
-            }
+        "data": sim.tolist(),
+        "params": {
+            "top_left_coords": {
+                "x": top_left_x,
+                "y": top_left_y
+            },
+            "size": size
         }
+    }
